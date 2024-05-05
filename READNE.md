fa4595f275a3567d6136d63d735ef96752c1f24f
Dolev Dublon <62290677+dolev146@users.noreply.github.com>
2024-05-05 18:59:40 +0300

    error need to check

---
+++ READNE.md
@@ -0,0 +1,17 @@
+# mpx-reader
+
+This project aims to solve the problem of reading data from an mpx file format.
+
+[stack-overflow-cargo-watch](https://stackoverflow.com/questions/29461693/how-can-i-get-cargo-to-recompile-changed-files-automatically)
+
+install cargo-watch:
+
+```rust
+    cargo install cargo-watch
+```
+
+to compile the code use the command in terminal:
+
+```rust
+    cargo watch -q -c -x 'run -q'
+```
---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![allow(unused)] // silence unused warnings while exploring (to comment out)
+
+use std::{fs, ptr::metadata};
+
+use walkdir::WalkDir;
+const DIR: &str = "./";
+
+fn main() {
+    println!("Hello, world!!!!");
+    
+    let mut total_size: u64 = 0;
+    let mut total_numbers: u32 = 0;
+
+    for entry in WalkDir::new(DIR).into_iter().filter_map(|e| e.ok()) {
+        let path = entry.path();
+        if path.is_file() && !entry.path_is_symlink() {
+            total_numbers += 1;
+            let metadata = fs::metadata(entry.path()).unwrap();
+            total_size += metadata.len();
+        }
+        // println!("{}", entry.path().display());
+    }
+}
+
+
+// used this youtube to start this project
+// https://www.youtube.com/watch?v=FzbxAhTqK9s&ab_channel=JeremyChone
